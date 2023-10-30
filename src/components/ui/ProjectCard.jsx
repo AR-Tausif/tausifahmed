6c5e799b4b32c443c0efee882e6a2d66349a804d
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2023-10-30 18:23:38 +0000

    adde hover card effect and trying to fetch portfolio data json

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,5 +1,8 @@
+import ProjectMiniCard from "./ProjectMiniCard";
+
 const PortfolioCard = ({small, work}) => {
-    const {name, title, thumbImg} = work;
+    // const {name, title, thumbImg} = work;
+    console.log(work)
     return ( 
         <div className={`${small ? 'h-[30vh]' : 'h-[100vh]'}
          w-full
@@ -8,12 +11,7 @@ const PortfolioCard = ({small, work}) => {
         relative
         overflow-hidden
         block
-        z-10
-                    
-        bg-[url('https://framerusercontent.com/images/idwwZIIqbma8TEQVuw7zr8Em7g.jpg?scale-down-to=2048')]
-        bg-cover
-        bg-no-repeat
-        bg-center
+    
 
         before:content-['']
         before:absolute
@@ -25,9 +23,10 @@ const PortfolioCard = ({small, work}) => {
         before:opacity-30
         before:z-[-5] rounded-2xl`}
         >
+            <ProjectMiniCard/>
             <div className="text-center text-white space-y-5 absolute bottom-8">
-            <h4 className={`font-bold ${small ? 'text-4xl' : "text-6xl"}`}>{name}</h4>
-            <h5 className={` text-center font-semibold mx-auto px-11 -tracking-wider leading-[1.2] ${small ? 'text-2xl' : "w-3/4 text-4xl"}`}>{title}</h5>
+            <h4 className={`font-bold ${small ? 'text-4xl' : "text-6xl"}`}>dfda</h4>
+            <h5 className={` text-center font-semibold mx-auto px-11 -tracking-wider leading-[1.2] ${small ? 'text-2xl' : "w-3/4 text-4xl"}`}>dkaksdfkd</h5>
             </div>
         </div>
      );
--- src/layout/pages/works/Works.jsx
+++ src/layout/pages/works/Works.jsx
@@ -4,16 +4,20 @@ import PortfolioCard from "../../../components/ui/ProjectCard";
 const Works = () => {
   const [works, setWorks] = useState([]);
   useEffect(()=>{
-    fetch("http://localhost:3000/portfolio")
+    fetch("https://backend-portfolio-lilac.vercel.app/portfolio")
     .then(res=> res.json())
-    .then(data=>setWorks(data))
+    .then(data=>{
+      console.log(data)
+      setWorks(data)
+    })
   },[])
+
     return ( 
         <section className="max-w-screen-xl mx-auto text-center py-20">
         <h3 className="text-[4rem] uppercase font-semibold py-24">Selected Works</h3>
         <div className="flex flex-col space-y-20">
         {
-          works.map((work, index)=><PortfolioCard key={index} work={work} /> )
+          works.map((work, index)=><PortfolioCard key={index} work={works} /> )
         }
         </div>
       </section>
